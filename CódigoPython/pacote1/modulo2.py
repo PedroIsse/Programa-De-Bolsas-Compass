b5be272f46b0b253e64a89446814b7b42795afa7
PedroIsse <pedroisse8@gmail.com>
2024-11-19 00:07:08 -0300

    Adicionando conteúdo: README Sprint03 e alguns exercícios Foram adicionados conteúdos aprendidos no curso de python e a realização de alguns exercícios de python

--- CódigoPython/pacote1/modulo2.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print('Rodando o main!')
-
-
-if __name__ == '__main__':
-    main()
--- CódigoPython/pacote_v5.py
+++
@@ -1,6 +0,0 @@
-from calc import soma, subtracao
-
-print('Soma', soma(3, 2))
-print('Subtração', subtracao(3, 2))
-
-# Um pacote que AGRUPA outros pacotes (Ótimo para a modularização do código)
